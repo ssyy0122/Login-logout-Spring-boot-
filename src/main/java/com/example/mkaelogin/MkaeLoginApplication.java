6f804f703d4e08dc73db5b2ce1a5e2e8941c7af8
ssyy0122 <z01039118740@gmail.com>
2022-05-17 16:04:29 +0900

    datebase연결

---
+++ src/main/java/com/example/mkaelogin/MkaeLoginApplication.java
@@ -0,0 +1,13 @@
+package com.example.mkaelogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MkaeLoginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MkaeLoginApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mkaelogin/MkaeLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mkaelogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MkaeLoginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
