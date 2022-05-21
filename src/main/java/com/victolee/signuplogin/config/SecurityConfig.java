f06f3b9d4fb5e3d016b71c4fa13f311de5f27e2e
ssyy0122 <z01039118740@gmail.com>
2022-05-21 18:09:16 +0900

    어노테이션 추가

--- src/main/java/com/victolee/signuplogin/config/SecurityConfig.java
+++ src/main/java/com/victolee/signuplogin/config/SecurityConfig.java
@@ -1,4 +1,12 @@
 package com.victolee.signuplogin.config;
 
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+@AllArgsConstructor
+
 public class SecurityConfig {
 }
