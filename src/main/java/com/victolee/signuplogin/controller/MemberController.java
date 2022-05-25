4831df1cddec9182450f4b8b5ceeb00526220a67
ssyy0122 <z01039118740@gmail.com>
2022-05-25 17:28:54 +0900

    mapping add

--- src/main/java/com/victolee/signuplogin/controller/MemberController.java
+++ src/main/java/com/victolee/signuplogin/controller/MemberController.java
@@ -1,10 +1,25 @@
 package com.victolee.signuplogin.controller;
 
+import com.victolee.signuplogin.service.MemberService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @AllArgsConstructor
 public class MemberController {
-    private MemberService memberService
+    private MemberService memberService;
+
+    //메인페이지
+    @GetMapping("/")
+    public String index() {
+        return "/index";
+    }
+    //회원가입 페이지
+    @GetMapping
+    public String dispSingnup() {
+        return "/singup";
+    }
+    //회원가입 처리
 }
